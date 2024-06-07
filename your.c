d94d7e06ae3167b82a4e5bfbc8ecd1f01bb11df9
hos8 <jam39so@outlook.com>
2024-06-07 23:17:48 +0800

    Modify your.c

--- your.c
+++ your.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Senior effort painting everyone central manage recently act.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
