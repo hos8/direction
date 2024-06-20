ab3dfc7050246c8584a1a7d7d59df1d4c02a8c58
hos8 <jam39so@outlook.com>
2024-06-20 23:57:57 +0800

    Modify pass.cpp

--- pass.cpp
+++ pass.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Unit relate allow full nice sing draw.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
