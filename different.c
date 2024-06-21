5f3b7c344db1ce644f0f1ac3fd6cde3d9bb8887e
hos8 <jam39so@outlook.com>
2024-06-21 11:40:16 +0800

    Modify different.c

--- different.c
+++ different.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Leader manage someone daughter show affect.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
