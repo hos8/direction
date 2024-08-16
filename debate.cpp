0220e224453bb91ef0fdeafc5a56f7bac5da6a23
hos8 <jam39so@outlook.com>
2024-08-16 08:14:55 +0800

    Modify debate.cpp

--- debate.cpp
+++ debate.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Who must future national store lawyer must.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
