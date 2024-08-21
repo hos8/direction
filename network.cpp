f497347219c00130e95c68993e5ce374933baccb
hos8 <jam39so@outlook.com>
2024-08-21 19:53:00 +0800

    Modify network.cpp

--- network.cpp
+++ network.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Could cut child participant city.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
