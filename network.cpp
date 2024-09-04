e82595bab596e9bc78cba77d593660651374e909
hos8 <jam39so@outlook.com>
2024-09-04 13:47:11 +0800

    Modify network.cpp

--- network.cpp
+++ network.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Always star hope reflect practice one.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
