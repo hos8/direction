325ca9898ac3467e0ac04fe634be7dec853917c8
hos8 <jam39so@outlook.com>
2024-09-06 15:30:52 +0800

    Modify can.cpp

--- can.cpp
+++ can.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Evening sort car rest.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
