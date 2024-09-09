a1e470ba9333075aa56fe8c69a48efc73f46a24d
hos8 <jam39so@outlook.com>
2024-09-09 21:21:35 +0800

    Modify network.cpp

--- network.cpp
+++ network.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Responsibility people consider guy fill assume.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
