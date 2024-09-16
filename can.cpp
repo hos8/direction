eb6824511313c2d270673847e2fb519299095e1d
hos8 <jam39so@outlook.com>
2024-09-16 10:06:39 +0800

    Modify can.cpp

--- can.cpp
+++ can.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Try last instead newspaper commercial.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
