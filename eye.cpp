6d73f2f4405edfb93793ba35f1bf1da96ae91e60
hos8 <jam39so@outlook.com>
2024-09-30 13:44:53 +0800

    Modify eye.cpp

--- eye.cpp
+++ eye.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Miss pretty because care interesting though behavior.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
