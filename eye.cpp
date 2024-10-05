b5cd90900b7ad86d6936278a0f052eb722acb23e
hos8 <jam39so@outlook.com>
2024-10-05 14:20:28 +0800

    Modify eye.cpp

--- eye.cpp
+++ eye.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Truth identify radio increase large make officer.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
