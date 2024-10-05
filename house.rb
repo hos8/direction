9b4a9436db8b0b79c7dc4b0ed4c7481e943440b1
hos8 <jam39so@outlook.com>
2024-10-05 14:20:28 +0800

    Delete house.rb

--- house.rb
+++
@@ -1,7 +0,0 @@
-puts "Hello, World!"
-
-# Near against social food wrong pattern individual brother.
-puts "Hello, World!"
-
-# Fact listen region tonight stuff smile school development.
-puts "Hello, World!"
