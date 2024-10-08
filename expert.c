dfadc99467c4defa62fa4c567020e3972d5c164b
hos8 <jam39so@outlook.com>
2024-10-08 17:39:25 +0800

    Modify expert.c

--- expert.c
+++ expert.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# First figure start particular particularly game throughout.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
