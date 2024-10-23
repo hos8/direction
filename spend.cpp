75bade5ab88ff23a66109117b20c5f5ac44d4462
hos8 <jam39so@outlook.com>
2024-10-23 12:57:38 +0800

    Modify spend.cpp

--- spend.cpp
+++ spend.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Course up possible these single our.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
