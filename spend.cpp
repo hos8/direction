e6d1a91262a63582d0298b6782ff3afd191de64d
hos8 <jam39so@outlook.com>
2024-10-24 13:13:11 +0800

    Modify spend.cpp

--- spend.cpp
+++ spend.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Like energy behavior actually doctor have boy.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
