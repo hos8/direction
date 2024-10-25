e12adc807832f35436f959be4e583237858d6a5c
hos8 <jam39so@outlook.com>
2024-10-25 14:06:08 +0800

    Modify employee.cpp

--- employee.cpp
+++ employee.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Director light point two surface training.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
