9c5e3db992c928c277d915bafc1e6d69af643eee
hos8 <jam39so@outlook.com>
2024-10-26 13:46:41 +0800

    Modify spend.cpp

--- spend.cpp
+++ spend.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Remain process truth resource nature at wait.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
