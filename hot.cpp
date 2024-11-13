a9f1f0bc51b6d14339c562b4f2dfaa7634c03448
hos8 <jam39so@outlook.com>
2024-11-13 14:44:58 +0800

    Modify hot.cpp

--- hot.cpp
+++ hot.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# System customer part dog officer husband.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
