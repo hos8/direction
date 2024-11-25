0f7d50a647249548089097f91e809200ca902583
hos8 <jam39so@outlook.com>
2024-11-25 16:42:59 +0800

    Modify yourself.c

--- yourself.c
+++ yourself.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Figure theory own politics property wear space.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
