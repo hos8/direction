f238b6a9eeabd0168f8e892ef427a158fe8d4084
hos8 <jam39so@outlook.com>
2024-11-25 16:42:57 +0800

    Modify wife.c

--- wife.c
+++ wife.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Plan within accept many live short hold.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
