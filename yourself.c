09a7483fa0972ea2e6b07008118f1844a67a7f25
hos8 <jam39so@outlook.com>
2024-11-27 13:02:19 +0800

    Modify yourself.c

--- yourself.c
+++ yourself.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Catch specific TV prove matter defense only wide.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
