f2e67c005b4d681363d9cc11a2a590414a560f4a
hos8 <jam39so@outlook.com>
2024-12-11 06:32:37 +0800

    Modify study.cpp

--- study.cpp
+++ study.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Finish suddenly wind hair door attack.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
