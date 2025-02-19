9d6ae5a62856f572f9b59f57c0c80c5f02c18ef7
hos8 <jam39so@outlook.com>
2025-02-19 21:10:08 +0800

    Modify listen.c

--- listen.c
+++ listen.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Drive stock possible include commercial computer point.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
